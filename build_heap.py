b6087e63f9cad837367dee3da4822f6db3f4447e
Kirils Baranovs <125561803+fierlon@users.noreply.github.com>
2023-03-19 22:35:09 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -3,40 +3,36 @@
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    n = len(data)
+    
+    def rec_heapify(i):
+        nonlocal swaps
+        min_index = i
+        left = 2*i + 1
+        right = 2*i + 2
 
+        if left < n and data[left] < data[min_index]:
+            min_index = left
 
+        if right < n and data[right] < data[min_index]:
+            min_index = right
+
+        if i != min_index:
+            swaps.append((i, min_index))
+            data[i], data[min_index] = data[min_index], data[i]
+            rec_heapify(min_index)
+            
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
